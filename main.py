db7d6ee061ed839acd29c2335f00b7c1096932b5
Mehta-Kartik <24cskar010@ldce.ac.in>
2026-04-08 12:16:58 +0530

    Modularized the Data Ingestion

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from src.TextSummarizer.logging import logger
-logger.info("Hello World")
+# logger.info("Hello World")
+
+from src.TextSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logger.info(f"{STAGE_NAME} initiated")
+    data_ingest_pipeline=DataIngestionTrainingPipeline()
+    data_ingest_pipeline.initiate_data_ingestion()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
+    print(e)
---
+++ src/TextSummarizer/config/configuration.py
@@ -0,0 +1,22 @@
+from src.TextSummarizer.constants import CONFIG_FILE_PATH,PARAM_FILE_PATH
+from src.TextSummarizer.utils.common import read_yaml,create_directories
+from src.TextSummarizer.entity import DataIngestionConfig
+
+class ConfigurationManager:
+    def __init__(self,config_path=CONFIG_FILE_PATH,params_path=PARAM_FILE_PATH):
+        self.config=read_yaml(config_path)
+        self.params=read_yaml(params_path)
+        
+        create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self)-> DataIngestionConfig:
+        config=self.config.data_ingestion
+        create_directories([config.root_dir])
+
+        data_ingestion_config=DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_url=config.source_url,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir,
+        )
+        return data_ingestion_config
